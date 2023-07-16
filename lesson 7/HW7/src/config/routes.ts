c103feeef085f3409bb677b91a9a3b68987b005d
Ahmar <ahmarkakimov2005@gmail.com>
2023-07-17 05:49:08 +0600

    Completed HW7

--- lesson 7/HW7/src/config/routes.ts
+++ lesson 7/HW7/src/config/routes.ts
@@ -1,1 +1,17 @@
-export default []
+export default [
+  {
+    name: 'HomePage',
+    path: '/',
+    component: () => import('@/views/HomePage.vue')
+  },
+  {
+    name: 'ContactPage',
+    path: '/contact',
+    component: () => import('@/views/ContactsPage.vue')
+  },
+  {
+    name: 'GaleryPage',
+    path: '/galery',
+    component: () => import('@/views/GaleryPage.vue')
+  },
+]
